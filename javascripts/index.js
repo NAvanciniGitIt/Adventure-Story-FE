ae80d5545d3018e4be02d76cf55d72d6db2beec4
Tasha <navancini@gmail.com>
2021-03-08 13:51:15 -0500

    added avatars

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,6 @@
-const user = ""
+let user = ""
+
+let avatar = ""
 
 function main() {
   return document.getElementById("main")
@@ -6,6 +8,14 @@ function main() {
 
 function resetMain() {
   main().innerHTML = ""
+}
+
+function nameInput() {
+  return document.getElementById("name")
+}
+
+function avatarInput() {
+  return document.getElementById("avatar")
 }
 
 function form() {
@@ -21,17 +31,64 @@ function nameTemplate() {
     <h3>Submit Your Name</h3>
     <form id ="form">
       <div class="input-field">
-        <label for="name">Name</label>
+        <label for="name">Name:</label>
         <input type="text" name="name" id="name">
       </div>
-  `
+      <input type="submit" value="Create Hero">
+    </form>  
+  `;
+}
+
+function avatarTemplate() {
+  return `
+  <h3>Choose your Avatar</h3> 
+  <form id="form">
+  <img src="avatars/Archer.png">
+  <input type="hidden" id="avatar" value="<img src='avatars/Archer.png'>">
+  <input type="submit" value="Choose">
+  <img src="avatars/Basic.png">
+  <input type="hidden" id="avatar" value="<img src='avatars/Basic.png'>">
+  <input type="submit" value="Choose">
+  <img src="avatars/Mage.png">
+  <input type="hidden" id="avatar" value="<img src='avatars/Mage.png'>">
+  <input type="submit" value="Choose">
+  <img src="avatars/Science.png">
+  <input type="hidden" id="avatar" value="<img src='avatars/Science.png'>">
+  <input type="submit" value="Choose">
+  <img src="avatars/Rogue.png">
+  <input type="hidden" id="avatar" value="<img src='avatars/Rogue.png'>">
+  <input type="submit" value="Choose">
+  <img src="avatars/Sword.png">
+  <input type="hidden" id="avatar" value="<img src='avatars/Sword.png'>">
+  <input type="submit" value="Choose">
+  `;
 }
 
 function renderNameTemplate() {
   resetMain()
   main().innerHTML = nameTemplate()
- 
+  form().addEventListener("submit", submitName)
 }
 
-document.addEventListener("DOMContentLoaded", function() { getUserName()
+function submitName(e) {
+  e.preventDefault()
+  user = nameInput().value
+  main().innerHTML = avatarTemplate()
+  form().addEventListener("submit", submitAvatar)
+
+}
+
+function submitAvatar(e) {
+  e.preventDefault()
+    avatar = avatarInput().value
+
+}
+
+document.addEventListener("DOMContentLoaded", function() { 
+  if(user == "") {
+    renderNameTemplate()
+  }
+  else {
+    main().innerHTML = avatarTemplate()
+  }
 })
