667e6569923dd6a5ed23e6a968709b2079e553a3
Tasha <navancini@gmail.com>
2021-03-08 12:38:30 -0500

    added form for username

--- javascripts/index.js
+++ javascripts/index.js
@@ -8,12 +8,29 @@ function resetMain() {
   main().innerHTML = ""
 }
 
+function form() {
+  return document.getElementById("form")
+}
+
 function getUserName(input) {
   user = input
 }
 
 function nameTemplate() {
-  
+  return `
+    <h3>Submit Your Name</h3>
+    <form id ="form">
+      <div class="input-field">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+      </div>
+  `
+}
+
+function renderNameTemplate() {
+  resetMain()
+  main().innerHTML = nameTemplate()
+ 
 }
 
 document.addEventListener("DOMContentLoaded", function() { getUserName()
