594735892b56200b1de68b6d18b1697a0b9651e5
Tasha <navancini@gmail.com>
2021-03-08 14:41:05 -0500

    updated choosing avatars

--- javascripts/index.js
+++ javascripts/index.js
@@ -14,8 +14,34 @@ function nameInput() {
   return document.getElementById("name")
 }
 
-function avatarInput() {
-  return document.getElementById("avatar")
+function archer() {
+  avatar = "<img src='avatars/Archer.png'>"
+  return false
+}
+
+function basic() {
+  avatar = "<img src='avatars/Basic.png'>"
+  return false
+}
+
+function mage() {
+  avatar = "<img src='avatars/Mage.png'>"
+  return false
+}
+
+function science() {
+  avatar = "<img src='avatars/Science.png'>"
+  return false
+}
+
+function rogue() {
+  avatar = "<img src='avatars/Rogue.png'>"
+  return false
+}
+
+function sword() {
+  avatar = "<img src='avatars/Sword.png'>"
+  return false
 }
 
 function form() {
@@ -28,8 +54,9 @@ function getUserName(input) {
 
 function nameTemplate() {
   return `
-    <h3>Submit Your Name</h3>
-    <form id ="form">
+    <h2>Welcome Adventurer!</h2>
+    <h3>Tell us your name!</h3>
+    <form id="form">
       <div class="input-field">
         <label for="name">Name:</label>
         <input type="text" name="name" id="name">
@@ -42,27 +69,27 @@ function nameTemplate() {
 function avatarTemplate() {
   return `
   <h3>Choose your Avatar</h3> 
-  <form id="form">
   <img src="avatars/Archer.png">
   <input type="hidden" id="avatar" value="<img src='avatars/Archer.png'>">
-  <input type="submit" value="Choose">
+  <input type="submit" value="Choose" onclick="return archer()">
   <img src="avatars/Basic.png">
   <input type="hidden" id="avatar" value="<img src='avatars/Basic.png'>">
-  <input type="submit" value="Choose">
+  <input type="submit" value="Choose" onclick="return basic()">
   <img src="avatars/Mage.png">
   <input type="hidden" id="avatar" value="<img src='avatars/Mage.png'>">
-  <input type="submit" value="Choose">
+  <input type="submit" value="Choose" onclick="return mage()">
   <img src="avatars/Science.png">
   <input type="hidden" id="avatar" value="<img src='avatars/Science.png'>">
-  <input type="submit" value="Choose">
+  <input type="submit" value="Choose" onclick="return science()">
   <img src="avatars/Rogue.png">
   <input type="hidden" id="avatar" value="<img src='avatars/Rogue.png'>">
-  <input type="submit" value="Choose">
+  <input type="submit" value="Choose" onclick="return rogue()">
   <img src="avatars/Sword.png">
   <input type="hidden" id="avatar" value="<img src='avatars/Sword.png'>">
-  <input type="submit" value="Choose">
+  <input type="submit" value="Choose" onclick="return sword()">
   `;
 }
+
 
 function renderNameTemplate() {
   resetMain()
@@ -70,18 +97,23 @@ function renderNameTemplate() {
   form().addEventListener("submit", submitName)
 }
 
+function renderAvatarTemplate() {
+  resetMain()
+  main().innerHTML = avatarTemplate()
+}
+
 function submitName(e) {
   e.preventDefault()
   user = nameInput().value
-  main().innerHTML = avatarTemplate()
-  form().addEventListener("submit", submitAvatar)
-
+  
+  renderAvatarTemplate()
 }
 
-function submitAvatar(e) {
-  e.preventDefault()
-    avatar = avatarInput().value
-
+function renderStory() {
+  resetMain()
+  return `
+    ${avatar}
+  `
 }
 
 document.addEventListener("DOMContentLoaded", function() { 
@@ -89,6 +121,6 @@ document.addEventListener("DOMContentLoaded", function() {
     renderNameTemplate()
   }
   else {
-    main().innerHTML = avatarTemplate()
+   renderStory
   }
 })
