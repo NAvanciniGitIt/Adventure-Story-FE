e73941028f3ebada21612044b1f9ffbd9f6ac908
Tasha <navancini@gmail.com>
2021-03-08 12:29:33 -0500

    added some functions

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,20 @@
-function name(params) {
+const user = ""
+
+function main() {
+  return document.getElementById("main")
+}
+
+function resetMain() {
+  main().innerHTML = ""
+}
+
+function getUserName(input) {
+  user = input
+}
+
+function nameTemplate() {
   
 }
+
+document.addEventListener("DOMContentLoaded", function() { getUserName()
+})
