05be3df2c4c50d37c5f12d491d8ee57d0692d4d2
Tasha <navancini@gmail.com>
2021-03-12 09:59:11 -0500

    added a capitalize function for names

--- javascripts/globals.js
+++ javascripts/globals.js
@@ -1,9 +1,12 @@
 /** Global Variables **/
 const baseUrl = "http://localhost:3000"
 let stories = [];
-let current_user = ""
-let current_story = ""
-let avatar = ""
+let current_user = "";
+let current_story = "";
+let avatar = "";
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
 const archerPic = "<img src='avatars/Archer.png'></img>"
 const basicPic = "<img src='avatars/Basic.png'>"
 const magePic = "<img src='avatars/Mage.png'>"
