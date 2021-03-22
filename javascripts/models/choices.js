dee8c78786a17e6bf05bf7cd88ce1c25e37d013b
Tasha <navancini@gmail.com>
2021-03-22 13:00:05 -0400

    added Choice for backend

--- javascripts/models/choices.js
+++ javascripts/models/choices.js
@@ -6,10 +6,44 @@ class Choice {
     this.name = attr.name;
     this.chosen = attr.chosen;
     this.user_id = attr.user_id;
-    this.story_id = attr.story_id
-    this.checkpoint_id = attr.checkpoint_id
+    this.story_id = attr.story_id;
+    this.checkpoint_id = attr.checkpoint_id;
     Choice.all.push(this)
   }
+
+  static create(attr) {
+    
+    let choice = new Choice(attr)
+    choice.save()
+    return choice
+    
+  }
+
+  save() {
+    Choice.all.push(this)
+  }
+ 
+  static createChoiceObj(decision, id) {
+
+    
+    let strongParams = {
+      choice: {
+        user_id: current_user.id,
+        story_id: current_user.id,
+        name: decision,
+        chosen: true,
+        checkpoint_id: id
+      }
+    }
+    
+    Api.post('/choices', strongParams)
+      .then(function(data) {
+        Choice.create(data)
+        current_checkpoint = data.checkpoint_id
+        current_story.check_points = current_checkpoint
+        
+      })
+    }
   
   
   // Templates
@@ -17,8 +51,8 @@ class Choice {
     
     return `
       <h3> Make a choice, ${current_user.name.capitalize()}! </h3> 
-        <p>${bus.name} <input type="hidden" id="bus" >&nbsp;<input type="submit" value="Choice 1" onclick="return ride()"></p> 
-        <p>${bar.name} <input type="hidden" id="bar" >&nbsp;<input type="submit" value="Choice 2" onclick="return walk()"></p>
+        <p> <input type="hidden" id="one" >&nbsp;<input type="submit" value="Choice 1" onclick="return c1()"></p> 
+        <p> <input type="hidden" id="two" >&nbsp;<input type="submit" value="Choice 2" onclick="return c2()"></p>
     `
     
   }
@@ -29,7 +63,3 @@ class Choice {
   
 }
 
-// Choice Variables
-
-let bus = new Choice({name: "Ride", chosen: false, checkpoint_id: 1})
-let bar = new Choice({name: "Walk", chosen: false, checkpoint_id: 2})
